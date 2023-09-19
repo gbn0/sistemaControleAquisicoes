6014fb3531bb8145b5153cefc0a47a128f4ed679
Gabriel Saldanha <130391676+gbn0@users.noreply.github.com>
2023-09-19 14:12:20 -0300

    Mudança de atributo do funcionário para usuário

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -1,23 +1,14 @@
-import java.util.ArrayList;
-
 public class Funcionario extends Usuario{
     
     private Departamento departamento;
-    private ArrayList<Pedido> pedidos;
 
     public Funcionario(int id, String nome, String iniciais, Departamento departamento) {
         super(id, nome, iniciais);
         this.departamento = departamento;
-        this.pedidos = new ArrayList<Pedido>();
     }
 
     public Departamento getDepartamento() {
         return departamento;
     }
-
-    public ArrayList<Pedido> getPedidos() {
-        return pedidos;
-    }
-
     
 }
--- src/Usuario.java
+++ src/Usuario.java
@@ -1,13 +1,17 @@
+import java.util.ArrayList;
+
 public abstract class Usuario {
     
     private int id;
     private String nome;
     private String iniciais;
+    private ArrayList<Pedido> pedidos;
     
     public Usuario(int id, String nome, String iniciais) {
         this.id = id;
         this.nome = nome;
         this.iniciais = iniciais;
+        this.pedidos = new ArrayList<Pedido>();
     }
 
     public int getId() {
@@ -22,4 +26,7 @@ public abstract class Usuario {
         return iniciais;
     }
 
+    public ArrayList<Pedido> getPedidos() {
+        return pedidos;
+    }
 }
