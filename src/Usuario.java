6438556ae65c29c3d374f23445606dff4f2308a4
Gabriel Saldanha <130391676+gbn0@users.noreply.github.com>
2023-09-19 13:28:43 -0300

    Implementação da classe usuário.

---
+++ src/Usuario.java
@@ -0,0 +1,25 @@
+public class Usuario {
+    
+    private int id;
+    private String nome;
+    private String iniciais;
+    
+    public Usuario(int id, String nome, String iniciais) {
+        this.id = id;
+        this.nome = nome;
+        this.iniciais = iniciais;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getIniciais() {
+        return iniciais;
+    }
+
+}
