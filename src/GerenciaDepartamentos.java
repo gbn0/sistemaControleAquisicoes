9e8f12616e8dd6ca4f7fb2d96ca159861aae7e0c
Athos <docsathos@gmail.com>
2023-09-19 17:54:18 -0300

    implementação das classes de gerenciamento v.1

--- src/GerenciaDepartamentos.java
+++ src/GerenciaDepartamentos.java
@@ -1,3 +1,21 @@
-public class GerenciaDepartamentos {
-    
+import java.util.ArrayList;
+
+public class GerenciaDepartamentos { //athos
+    private ArrayList<Departamento> departamentos;
+    public GerenciaDepartamentos(){
+        departamentos = new ArrayList<Departamento>();
+    }
+
+    public boolean cadastraDepartamento(Departamento novoDepartamento){
+        return departamentos.add(novoDepartamento);
+    }
+
+    public Departamento pesquisaDepartamento(String nome){
+        for ( Departamento d : departamentos){
+            if(d.getNome().equals(nome)){
+                return d;
+            }
+        }
+        return null;
+    }
 }
--- src/GerenciaItens.java
+++ src/GerenciaItens.java
@@ -1,3 +1,21 @@
-public class GerenciaItens {
-    
+import java.util.ArrayList;
+
+public class GerenciaItens { //athos
+    private ArrayList<Item> itens;
+    public GerenciaItens(){
+        itens = new ArrayList<Item>();
+    }
+
+    public boolean cadastraItem(Item novoItem){
+        return itens.add(novoItem);
+    }
+
+    public Item pesquisaItem(String descricao){
+        for (Item i : itens){
+            if(i.getDescricao().equals(descricao)){
+                return i;
+            }
+        }
+        return null;
+    }
 }
