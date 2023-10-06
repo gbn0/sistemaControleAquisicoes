69383b6cbf22665e290257291a353f03fe7912c0
gabrielpetry23 <gabriel.petry23@gmail.com>
2023-10-06 19:15:09 -0300

    metodo add usuario

--- src/GerenciaUsuarios.java
+++ src/GerenciaUsuarios.java
@@ -1,3 +1,15 @@
+import java.util.ArrayList;
+
 public class GerenciaUsuarios {
+    private ArrayList<Usuario> usuarios;
     
+    public GerenciaUsuarios () {
+        usuarios = new ArrayList<>();
+    }
+
+    public void adicionaUsuario (Usuario u)  {
+        usuarios.add(u);
+    }
+
+    //pesquisaUsuario pelo id e nome
 }
