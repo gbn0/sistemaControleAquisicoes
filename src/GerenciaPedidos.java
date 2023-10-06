83efbc92d90e1fb676c3e8d5b4b584de65f1195c
Athos <docsathos@gmail.com>
2023-10-06 17:53:25 -0300

    commit pra classe buscar pedido pela desc

--- src/GerenciaPedidos.java
+++ src/GerenciaPedidos.java
@@ -1,3 +1,17 @@
-public class GerenciaPedidos {
+
+    //metodo para buscar pedidos pela descrição qnd a classe for implementada 
+    public ArrayList<Pedido> buscaPedidoDesc(String descricao){
+        ArrayList<Pedido> pedidoDescricao = new ArrayList<Pedido>();   
+        for (Pedido p : pedidos){
+            ArrayList <Item> v = p.getItens();
+            for (Item i : v){
+                if (i.equals(descricao)){
+                    pedidoDescricao.add(p);
+                }
+            }
+        }
+        return pedidoDescricao;
+    }
+
     
-}
+
